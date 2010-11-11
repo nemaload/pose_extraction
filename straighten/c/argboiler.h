e43e6f882701c6ee62b517de616d765dcd8fb6e3
David Dalrymple <davidad@alum.mit.edu>
2010-11-11 07:39:58 -0500

    ARGBOILER: better support for file arguments

--- straighten/c/argboiler.h
+++ straighten/c/argboiler.h
@@ -1,26 +1,28 @@
 #include <argtable2.h>
 
-#define ARG_CONSTS_(type,str,meth,lval,id,shrt,lng,desc,def) type id##_default;
+#define ARG_CONSTS_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) type id##_default;
 #define ARG_CONSTS(type,id,shrt,lng,desc,def) ARG_CONSTS_(type,id,shrt,lng,desc,def)
-#define ARG_STRUCT_(type,str,meth,lval,id,shrt,lng,desc,def) type id;
+#define ARG_STRUCT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) type id;
 #define ARG_STRUCT(type,id,shrt,lng,desc,def) ARG_STRUCT_(type,id,shrt,lng,desc,def)
-#define ARG_TABVAL_(type,str,meth,lval,id,shrt,lng,desc,def) struct arg_##str * id = arg_##str##meth(shrt,lng,"<" #str ">", desc " (default " #def ")");
+#define ARG_TABVAL_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) struct arg_##strct * id = arg_##strct##meth(shrt,lng,typestr, desc " (default " #def ")");
 #define ARG_TABVAL(type,id,shrt,lng,desc,def) ARG_TABVAL_(type,id,shrt,lng,desc,def)
-#define ARG_TABLE_(type,str,meth,lval,id,shrt,lng,desc,def) id,
+#define ARG_TABLE_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) id,
 #define ARG_TABLE(type,id,shrt,lng,desc,def) ARG_TABLE_(type,id,shrt,lng,desc,def)
-#define ARG_TABINIT_(type,str,meth,lval,id,shrt,lng,desc,def) id->lval = id##_default = def;
+#define ARG_TABINIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) id->lval = id##_default = def;
 #define ARG_TABINIT(type,id,shrt,lng,desc,def) ARG_TABINIT_(type,id,shrt,lng,desc,def)
-#define ARG_INIT_(type,str,meth,lval,id,shrt,lng,desc,def) params->id = id->lval;
+#define ARG_INIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) params->id = id->lval;
 #define ARG_INIT(type,id,shrt,lng,desc,def) ARG_INIT_(type,id,shrt,lng,desc,def)
 
-#define ARG_STR0 const char*,str,0,sval[0]
-#define ARG_STR1 const char*,str,1,sval[0]
-#define ARG_INT0 int,int,0,ival[0]
-#define ARG_INT1 int,int,1,ival[0]
-#define ARG_DBL0 double,dbl,0,dval[0]
-#define ARG_DBL1 double,dbl,1,dval[0]
-#define ARG_LIT0 int,lit,_0,count
-#define ARG_LITN int,lit,_n,count
+#define ARG_FIL0 const char*,str,"<file>",0,sval[0]
+#define ARG_FIL1 const char*,str,"<file>",1,sval[0]
+#define ARG_STR0 const char*,str,"<string>",0,sval[0]
+#define ARG_STR1 const char*,str,"<string>",1,sval[0]
+#define ARG_INT0 int,int,"<int>",0,ival[0]
+#define ARG_INT1 int,int,"<int>",1,ival[0]
+#define ARG_DBL0 double,dbl,"<real>",0,dval[0]
+#define ARG_DBL1 double,dbl,"<real>",1,dval[0]
+#define ARG_LIT0 int,lit,"",_0,count
+#define ARG_LITN int,lit,"",_n,count
 
 struct arg_lit* arg_lit_n(const char* shortopts, const char* longopts, const char* datatype, const char* glossary) {
   return arg_litn(shortopts,longopts,0,10,glossary);
