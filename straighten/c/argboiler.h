0e1e16a8495ac3884eb9cb3cb6de26bb1f946015
David Dalrymple <davidad@alum.mit.edu>
2010-11-12 17:03:48 -0500

    Various restructuring preparing for move to XCB

--- straighten/c/argboiler.h
+++ straighten/c/argboiler.h
@@ -1,3 +1,4 @@
+#ifndef ARGBOILER_
 #include <argtable2.h>
 
 #define ARG_CONSTS_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) type id##_default;
@@ -23,64 +24,12 @@
 #define ARG_DBL1 double,dbl,"<real>",1,dval[0]
 #define ARG_LIT0 int,lit,"",_0,count
 #define ARG_LITN int,lit,"",_n,count
-
-struct arg_lit* arg_lit_n(const char* shortopts, const char* longopts, const char* datatype, const char* glossary) {
-  return arg_litn(shortopts,longopts,0,10,glossary);
-}
-
-struct arg_lit* arg_lit_0(const char* shortopts, const char* longopts, const char* datatype, const char* glossary) {
-  return arg_lit0(shortopts,longopts,glossary);
-}
-
-ARGBOILER(ARG_CONSTS)
+#define ARGBOILER_
 
 typedef struct {
   ARGBOILER(ARG_STRUCT)
 } args_t;
 
-int parse_args(int argc, char** argv, args_t* params) {
-  /*
-   * We use the argtable library to parse arguments. The first step is
-   * building the titular argtable. This is the third place.
-   */
-  ARGBOILER(ARG_TABVAL)
-  struct arg_end* end = arg_end(10);
-  /*
-   * Don't forget to actually add your new args into the argtable below
-   * before the "end" sentinel.
-   */
-  void* argtable[] = {ARGBOILER(ARG_TABLE) end};
-  int nerrors;
+int parse_args(int argc, char** argv, args_t* params);
 
-  /*
-   * Here's the fifth place, where we fill in the defaults prior to
-   * running the arg_parse function.
-   */
-
-  ARGBOILER(ARG_TABINIT)
-
-  nerrors = arg_parse(argc, argv, argtable);
-  if(nerrors > 0) {
-    printf("\e[A");
-    arg_print_errors(stderr, end, argv[0]);
-    fprintf(stderr,"\nUsage:\n%s",argv[0]);
-    arg_print_syntaxv(stderr, argtable, "\n\n");
-    arg_print_glossary(stderr, argtable, "\t%-25s %s\n");
-    fprintf(stderr,"\n");
-    exit(nerrors);
-  } else {
-    /*
-     * Finally, here we fill in the "params" structure.
-     *
-     * TODO:
-     * There might be some way to handle these five needs
-     * while specifying arguments in only one place, by
-     * some very clever macro trickery, involving using a
-     * functional macro name as the argument to a functional
-     * macro, and defining such a higher-order macro with
-     * all the param data. I'll get to that later...
-     */
-    ARGBOILER(ARG_INIT)
-    return 0;
-  }
-}
+#endif
--- straighten/c/straighten.c
+++ straighten/c/straighten.c
@@ -28,36 +28,11 @@
 /*
  * I wrote a little C macro hackery called ARGBOILER,
  * which uses the argtable2 library to parse arguments.
- * This is the sligtly awkward, but comparatively concise,
- * argument specification list.
+ * The specification list is in argboiler_args.h, and
+ * is quite concise.
  */
 
-#define ARGBOILER(ARG) \
-  ARG(ARG_FIL1,input_filename,NULL,NULL,"the input image (as raw data)",NULL) \
-  ARG(ARG_INT1,input_width,"w","width","the width of each input image slice",-1) \
-  ARG(ARG_INT1,input_height,"h","height","the height of each input image slice",-1) \
-  ARG(ARG_INT0,output_width,NULL,"worm-width","the width of the output image",-1) \
-  ARG(ARG_INT0,output_height,NULL,"worm-height","the height of the output image",-1) \
-  ARG(ARG_INT0,output_extension,NULL,"worm-extension","how many pixels on either edge of the backbone to include (head and tail)",-1) \
-  ARG(ARG_FIL0,output_filename,"o","output","the output image (as raw data)",NULL) \
-  ARG(ARG_INT0,sd_sample_size,NULL,"sdss","the sample size for computing standard deviation",1000) \
-  ARG(ARG_DBL0,thresh_sds,"t","thresh","the number of standard deviations above mean makes a pixel considered part of the worm",0.5) \
-  ARG(ARG_INT0,mst_sample_size,NULL,"mstss","the sample size for making the MST",120) \
-  ARG(ARG_INT0,refine_sample_size,NULL,"rfss","the sample size of E_image in refining the backbone",3000) \
-  ARG(ARG_INT0,refine_refresh_size,NULL,"rfrs","the number of E_image samples to replace each iteration",10) \
-  ARG(ARG_DBL0,refine_threshhold,NULL,"rfth","the average distance (in pixels) points must move less than to terminate refinement",1.55) \
-  ARG(ARG_INT0,restart_iterations,NULL,"rfri","if the refinement doesn't converge after this many iterations, restart with a new MST",1000) \
-  ARG(ARG_INT0,delta_history,NULL,"rfdh","the number of iterations the points must move very little in a row to count",150) \
-  ARG(ARG_LIT0,spread_voronoi,"3","spread","adjust control points using the nearest pixels of neighboring control points as well as their own",0) \
-  ARG(ARG_LIT0,use_brightness,"r","weight","weight E_image by pixel brightness instead of just threshholding",0) \
-  ARG(ARG_LIT0,no_interpolate,"c","no-interpolate","don't interpolate input pixels when restacking",0) \
-  ARG(ARG_DBL0,alpha,"a","alpha","weight of E_image; 1 in the original paper",0.15) \
-  ARG(ARG_DBL0,beta,"b","beta","weight of E_length; 0.5 in the original paper",1.1) \
-  ARG(ARG_DBL0,gamma,"g","gamma","weight of E_smoothness; 0.5 in the original paper",0.6) \
-  ARG(ARG_DBL0,delta,"d","delta","'inertia' term (not in the original paper)",2.2) \
-  ARG(ARG_DBL0,image_scale,"s","scale","factor to divide image width and height by in display",4.0) \
-  ARG(ARG_LITN,precache,"p","precache","mmap the image and read all the pixels; performing no processing if specified twice.",0) \
-
+#include "argboiler_args.h"
 #include "argboiler.h"
 
 /*
