addd9874dcef06066c8ce6b22b7fe88fec2e7af8
David Dalrymple (on tzadi) <davidad@alum.mit.edu>
2010-11-13 08:45:20 -0500

    Tweaking args

--- straighten/c/argboiler.h
+++ straighten/c/argboiler.h
@@ -1,29 +1,40 @@
 #ifndef ARGBOILER_
 #include <argtable2.h>
 
-#define ARG_CONSTS_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) type id##_default;
-#define ARG_CONSTS(type,id,shrt,lng,desc,def) ARG_CONSTS_(type,id,shrt,lng,desc,def)
-#define ARG_STRUCT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) type id;
-#define ARG_STRUCT(type,id,shrt,lng,desc,def) ARG_STRUCT_(type,id,shrt,lng,desc,def)
-#define ARG_TABVAL_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) struct arg_##strct * id = arg_##strct##meth(shrt,lng,typestr, desc " (default " #def ")");
-#define ARG_TABVAL(type,id,shrt,lng,desc,def) ARG_TABVAL_(type,id,shrt,lng,desc,def)
-#define ARG_TABLE_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) id,
-#define ARG_TABLE(type,id,shrt,lng,desc,def) ARG_TABLE_(type,id,shrt,lng,desc,def)
-#define ARG_TABINIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) id->lval = id##_default = def;
-#define ARG_TABINIT(type,id,shrt,lng,desc,def) ARG_TABINIT_(type,id,shrt,lng,desc,def)
-#define ARG_INIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def) params->id = id->lval;
-#define ARG_INIT(type,id,shrt,lng,desc,def) ARG_INIT_(type,id,shrt,lng,desc,def)
+#define ARG_CONSTS_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) type id##_default;
+#define ARG_CONSTS(type,id,shrt,lng,desc,def) ARG_CONSTS_(type,id,shrt,lng,desc,def,type##_DEF(def))
+#define ARG_STRUCT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) type id;
+#define ARG_STRUCT(type,id,shrt,lng,desc,def) ARG_STRUCT_(type,id,shrt,lng,desc,def,type##_DEF(def))
+#define ARG_TABVAL_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) struct arg_##strct * id = arg_##strct##meth(shrt,lng,typestr, desc defstr);
+#define ARG_TABVAL(type,id,shrt,lng,desc,def) ARG_TABVAL_(type,id,shrt,lng,desc,def,type##_DEF(def))
+#define ARG_TABLE_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) id,
+#define ARG_TABLE(type,id,shrt,lng,desc,def) ARG_TABLE_(type,id,shrt,lng,desc,def,type##_DEF(def))
+#define ARG_TABINIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) id->lval = id##_default = def;
+#define ARG_TABINIT(type,id,shrt,lng,desc,def) ARG_TABINIT_(type,id,shrt,lng,desc,def,type##_DEF(def))
+#define ARG_INIT_(type,strct,typestr,meth,lval,id,shrt,lng,desc,def,defstr) params->id = id->lval;
+#define ARG_INIT(type,id,shrt,lng,desc,def) ARG_INIT_(type,id,shrt,lng,desc,def,type##_DEF(def))
 
 #define ARG_FIL0 const char*,str,"<file>",0,sval[0]
+#define ARG_FIL0_DEF(d) ""
 #define ARG_FIL1 const char*,str,"<file>",1,sval[0]
+#define ARG_FIL1_DEF(d) ""
 #define ARG_STR0 const char*,str,"<string>",0,sval[0]
+#define ARG_STR0_DEF(d) " [default: " d "]"
 #define ARG_STR1 const char*,str,"<string>",1,sval[0]
+#define ARG_STR1_DEF(d) ""
 #define ARG_INT0 int,int,"<int>",0,ival[0]
+#define ARG_INT0_DEF(d) " [default: " #d "]"
 #define ARG_INT1 int,int,"<int>",1,ival[0]
+#define ARG_INT1_DEF(d) ""
 #define ARG_DBL0 double,dbl,"<real>",0,dval[0]
+#define ARG_DBL0_DEF(d) " [default: " #d "]"
 #define ARG_DBL1 double,dbl,"<real>",1,dval[0]
+#define ARG_DBL1_DEF(d) ""
 #define ARG_LIT0 int,lit,"",_0,count
+#define ARG_LIT0_DEF(d) ""
 #define ARG_LITN int,lit,"",_n,count
+#define ARG_LITN_DEF(d) ""
+
 #define ARGBOILER_
 
 typedef struct {
--- straighten/c/argboiler_args.h
+++ straighten/c/argboiler_args.h
@@ -1,15 +1,13 @@
 #ifndef ARGBOILER_ARGS
 #define ARGBOILER_ARGS
 
+static const int automatic=-1;
+
 #define ARGBOILER(ARG) \
-  ARG(ARG_FIL1,input_filename,NULL,NULL,"the input image (as raw data)",NULL) \
+  ARG(ARG_FIL1,input_filename,NULL,NULL,"the input image (as raw data)","") \
   ARG(ARG_INT1,input_width,"w","width","the width of each input image slice",-1) \
   ARG(ARG_INT1,input_height,"h","height","the height of each input image slice",-1) \
-  ARG(ARG_INT0,output_width,NULL,"out-width","the width of the output image",-1) \
-  ARG(ARG_INT0,output_height,NULL,"out-height","the height of the output image",-1) \
-  ARG(ARG_INT0,output_extension,NULL,"out-extension","how many pixels on either edge of the backbone to include (head and tail)",-1) \
-  ARG(ARG_INT0,output_slice,NULL,"out-slice","output a 2D slice at the specified depth",-1) \
-  ARG(ARG_FIL0,output_filename,"o","output","the output image (as raw data)",NULL) \
+  ARG(ARG_LITN,precache,"p","precache","mmap the image and read all the pixels; performing no processing if specified twice.",0) \
   ARG(ARG_INT0,sd_sample_size,NULL,"sdss","the sample size for computing standard deviation",1000) \
   ARG(ARG_DBL0,thresh_sds,"t","thresh","the number of standard deviations above mean makes a pixel considered part of the worm",0.5) \
   ARG(ARG_INT0,mst_sample_size,NULL,"mstss","the sample size for making the MST",120) \
@@ -20,13 +18,17 @@
   ARG(ARG_INT0,delta_history,NULL,"rfdh","the number of iterations the points must move very little in a row to count",150) \
   ARG(ARG_LIT0,spread_voronoi,"3","spread","adjust control points using the nearest pixels of neighboring control points as well as their own",0) \
   ARG(ARG_LIT0,use_brightness,"r","weight","weight E_image by pixel brightness instead of just threshholding",0) \
-  ARG(ARG_LIT0,no_interpolate,"c","no-interpolate","don't interpolate input pixels when restacking",0) \
   ARG(ARG_DBL0,alpha,"a","alpha","weight of E_image; 1 in the original paper",0.15) \
   ARG(ARG_DBL0,beta,"b","beta","weight of E_length; 0.5 in the original paper",1.1) \
   ARG(ARG_DBL0,gamma,"g","gamma","weight of E_smoothness; 0.5 in the original paper",0.6) \
   ARG(ARG_DBL0,delta,"d","delta","'inertia' term (not in the original paper)",2.2) \
   ARG(ARG_DBL0,image_scale,"s","scale","factor to divide image width and height by in display",4.0) \
   ARG(ARG_INT0,x_delay_ms,"x","xdelay","milliseconds to wait at the end of step 7 before closing the window",0) \
-  ARG(ARG_LITN,precache,"p","precache","mmap the image and read all the pixels; performing no processing if specified twice.",0) \
+  ARG(ARG_LIT0,no_interpolate,"c","no-interp","don't interpolate input pixels when restacking",0) \
+  ARG(ARG_FIL0,output_filename,"o","output","the output image (as raw data) [default: input+'.out']","") \
+  ARG(ARG_INT0,output_width,NULL,"out-width","the width of the output image",automatic) \
+  ARG(ARG_INT0,output_height,NULL,"out-height","the height of the output image",automatic) \
+  ARG(ARG_INT0,output_extension,NULL,"out-extra","how many pixels on either edge of the backbone to include (head and tail)",automatic) \
+  ARG(ARG_INT0,output_slice,NULL,"out-slice","output a 2D slice at the specified depth",automatic) \
 
 #endif
